425ce38e9b7bea9cbbbb6a51918c758d17549463
isabellavjs <isabellavjs@gmail.com>
2020-11-17 16:12:39 -0300

    Cria o componente Table

---
+++ src/components/Table.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the starwars database!</h1>
+      </header>
+      <div>
+        <table></table>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
