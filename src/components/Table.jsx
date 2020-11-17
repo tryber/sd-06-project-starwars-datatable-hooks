214ec1ef4f6521f06dfcf2fccdd2712c10eb0bf5
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-11-17 20:57:30 -0300

    Adicionar Requisito 1

---
+++ src/components/Table.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+function Table() {
+  const { dataAPI, getData } = useContext(StarWarsContext);
+  const tableHeaders = ['Name', 'Rotation Period', 'Orbital Periode',
+    'Diameter', 'Climate', 'Gravity', 'Terrain', 'Surface Water',
+    'Population', 'Residents', 'Films', 'Created', 'Edited',
+  ];
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          { tableHeaders.map((title, index) => <th key={ `title${index}` }>{title}</th>) }
+        </tr>
+      </thead>
+      <tbody>
+        {dataAPI.map((obj, index) => {
+          const tableRows = (
+            <tr key={ `planet${index}` }>
+              <td>{obj.name}</td>
+              <td>{obj.rotation_period}</td>
+              <td>{obj.orbital_period}</td>
+              <td>{obj.diameter}</td>
+              <td>{obj.climate}</td>
+              <td>{obj.gravity}</td>
+              <td>{obj.terrain}</td>
+              <td>{obj.surface_water}</td>
+              <td>{obj.population}</td>
+              <td>{obj.residents}</td>
+              <td>{obj.films}</td>
+              <td>{obj.created}</td>
+              <td>{obj.edited}</td>
+            </tr>
+          );
+          return tableRows;
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import fetchAPI from '../services/fetchData';
 import StarWarsContext from './StarWarsContext';
 
-const Provider = ({ children }) => {
+function Provider({ children }) {
+  const [dataAPI, setData] = useState([]);
+
+  const getData = async () => {
+    const resuts = await fetchAPI();
+    setData(resuts);
+  };
+
   return (
-    <StarWarsContext.Provider>
+    <StarWarsContext.Provider value={ { dataAPI, getData } }>
       {children}
     </StarWarsContext.Provider>
   );
 }
 
 export default Provider;
+
+Provider.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
