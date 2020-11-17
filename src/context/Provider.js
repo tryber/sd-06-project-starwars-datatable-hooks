bb120c21ff47a84fac22f40284f6cb355e5f42b9
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-11-17 19:56:25 -0300

    refactoring

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import MyContext from './MyContext';
+import PropTypes from 'prop-types';
 import usePlanets from './usePlanets';
 
 const Provider = ({ children }) => {
-  const [ isFetching, setFetching ] = useState(true);
+  const [isFetching, setFetching] = useState(true);
   console.log(isFetching);
 
   const data = usePlanets();
@@ -12,13 +13,17 @@ const Provider = ({ children }) => {
   console.log(isFetching);
 
   if (isFetching) {
-    return ( <div>loading...</div> )
+    return (<div>loading...</div>);
   }
   return (
-    <MyContext.Provider value={data}>
+    <MyContext.Provider value={ data }>
       {children}
     </MyContext.Provider>
   );
 };
+
+Provider.proptypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export default Provider;
