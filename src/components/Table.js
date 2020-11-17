001e8e6caca9e2efca0bed477145a2177e5eb386
Julia Porto <juportob@gmail.com>
2020-11-17 19:31:40 -0200

    1st requirement - eslint

--- src/components/Table.js
+++ src/components/Table.js
@@ -29,7 +29,7 @@ function Table() {
       </thead>
       <tbody>
         {data.map((planet) => (
-          <tr key={ planet}>
+          <tr key={ planet }>
             <td key={ planet.name }>{ planet.name }</td>
             <td key={ planet.rotation_period }>{ planet.rotation_period }</td>
             <td key={ planet.orbital_period }>{ planet.orbital_period }</td>
