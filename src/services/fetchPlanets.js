c9a771988e0952fb2d2e6586387bb44aa927383f
LeandroParisi <lepcbelisario@gmail.com>
2020-11-17 16:05:24 -0300

    :sparkles: Requisito 1: Inicializado

---
+++ src/services/fetchPlanets.js
@@ -0,0 +1,9 @@
+async function fetchPlanets() {
+  const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const response = await fetch(URL);
+  const data = await response.json();
+
+  return data;
+}
+
+export default fetchPlanets;
