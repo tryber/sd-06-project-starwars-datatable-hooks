dcd66ddb6a5e69c81eef4a5b91f7549a12ca091f
William Gomes <williamjog@hotmail.com>
2020-11-17 17:31:34 -0300

    Forgot to add Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div className="title">
+      <h1> Star Wars </h1>
+      <h4> Planets Search Engine </h4>
+    </div>
+  );
+}
+
+export default Title;
