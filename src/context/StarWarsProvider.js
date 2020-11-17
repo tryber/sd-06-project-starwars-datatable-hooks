5003d9b961a361e9ae44c8274fee115b78284af4
labelle <beelsdelonge@gmail.com>
2020-11-17 19:31:39 -0300

    Arrumando o Lint

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -6,8 +6,8 @@ function StarWarsProvider() {
   const [planets, setPlanets] = useState([]);
 
   const serviceAPI = async () => {
-		const responseAPI = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-		const json = await responseAPI.json();
+    const responseAPI = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const json = await responseAPI.json();
 
     return setPlanets(json.results);
   };
