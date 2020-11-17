22df773ad86787acd4cd8cdab1714f2b0717fb2f
Bruna Franciele <brunafranciele1902@gmail.com>
2020-11-17 19:38:17 -0300

    envio do primeiro requisito

--- src/services/API.js
+++ src/services/API.js
@@ -2,11 +2,6 @@ const fetchStarWars = async () => {
   const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
   const json = await response.json();
   const lists = json.results;
-  // const filteredList = lists.map(
-  //   (list) => Object.entries(list).filter(
-  //     (key) => key[0] !== 'residents',
-  //   ),
-  // );
   return lists;
 };
 
