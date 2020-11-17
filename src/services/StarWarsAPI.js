13526891521bbe83a296d1f39241f15e19a2c341
Tiago Berwanger <berwangertiago@gmail.com>
2020-11-17 19:10:08 -0300

    Requirement 1: complete

--- src/services/StarWarsAPI.js
+++ src/services/StarWarsAPI.js
@@ -1,9 +1,9 @@
 // Faça uma requisição para o endpoint /planets da API de Star Wars e preencha uma tabela com os dados retornados, com exceção dos da coluna residents.
 
-// const StarWarsAPI = async () => {
-//   const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-//   const data = await response.json();
-//   return data;
-// };
+const StarWarsAPI = async () => {
+  const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+  const data = await response.json();
+  return data;
+};
 
-// export default StarWarsAPI;
+export default StarWarsAPI;
