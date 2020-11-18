53a7277e2e8f2dace978319e1aa663ca90fa3bf4
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-11-18 02:16:01 -0300

    add Provider, Table.js

---
+++ src/context/StarWarsProvider.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import StarWarsContext from './StarWarsContext';
+
+function StarWarsProvider({ children }) {
+  const [data, setData] = useState(['oi']);
+  const contextValue = { data, setData };
+  return (
+    <StarWarsContext.Provider value={ contextValue }>
+      {children}
+    </StarWarsContext.Provider>
+  );
+}
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default StarWarsProvider;
