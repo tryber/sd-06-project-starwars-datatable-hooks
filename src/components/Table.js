b1fa7ba88eb1a1f14445f9132690461adbe46f03
Alvaro Neto <alvaroshn@gmail.com>
2020-11-18 16:47:29 -0300

    Alteracoes req 3  [0]

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,45 +1,24 @@
 import React, { useContext, useEffect } from 'react';
-import StarWarsContext from '../context/StarWarsContext';
 import TableHeader from './TableHeader';
-import InputText from './InputText';
+import SearchInput from './SearchInput';
+import FilterByName from './FilterByName';
+import FilterHelper from '../services/FilterHelper';
+import StarWarsContext from '../context/StarWarsContext';
 
 function Table() {
-  const { data, fetchAPI, inputName } = useContext(StarWarsContext);
+  const { fetchAPI, willFilter } = useContext(StarWarsContext);
 
   useEffect(() => {
     fetchAPI();
   }, []);
 
-  const tableBody = data.map((item) => {
-    delete item.residents;
-    return item;
-  });
-
   return (
     <div>
-      <InputText />
+      <SearchInput />
       <table>
         <TableHeader />
         <tbody>
-          {tableBody
-            .filter((item) => item.name.toLowerCase().includes(inputName.toLowerCase()))
-            .map((e, i) => (
-              <tr key={ i }>
-                <td>{e.name}</td>
-                <td>{e.rotation_period}</td>
-                <td>{e.orbital_period}</td>
-                <td>{e.diameter}</td>
-                <td>{e.climate}</td>
-                <td>{e.gravity}</td>
-                <td>{e.terrain}</td>
-                <td>{e.surface_water}</td>
-                <td>{e.population}</td>
-                <td>{e.films}</td>
-                <td>{e.created}</td>
-                <td>{e.edited}</td>
-                <td>{e.url}</td>
-              </tr>
-            ))}
+          {willFilter ? <FilterHelper /> : <FilterByName />}
         </tbody>
       </table>
     </div>
--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -6,10 +6,14 @@ import StarWarsContext from './StarWarsContext';
 import { getPlanetList } from '../services/StarWarsAPI';
 
 const StarWarsProvider = ({ children }) => {
-  const initialObj = { filterByName: { name: '' } };
+  const initialObj = {
+    filterByName: { name: '' },
+    filterByNumericValues: [{ column: '', comparison: '', value: '' }],
+  };
 
   const [data, setData] = useState([]);
   const [filters, setFilters] = useState(initialObj);
+  const [willFilter, setWillFilter] = useState(false);
 
   const fetchAPI = async () => {
     getPlanetList().then((json) => {
@@ -17,11 +21,28 @@ const StarWarsProvider = ({ children }) => {
     });
   };
 
+  const handleToggleFilter = () => {
+    setWillFilter(true);
+  };
+
   const setFilterByName = (value) => {
     setFilters({
       ...filters,
       filterByName: { name: value },
     });
+    setWillFilter(false);
+  };
+
+  const setFilterByNumericValues = (column, comparison, value) => {
+    setFilters({
+      ...filters,
+      filterByNumericValues: { // filters.filterByNumericValues.concat({
+        column,
+        comparison,
+        value,
+      }, // ),
+    });
+    handleToggleFilter();
   };
 
   const context = {
@@ -32,6 +53,11 @@ const StarWarsProvider = ({ children }) => {
     inputName: filters.filterByName.name,
     setFilters,
     setFilterByName,
+    setFilterByNumericValues,
+    column: filters.filterByNumericValues.column,
+    comparison: filters.filterByNumericValues.comparison,
+    value: filters.filterByNumericValues.value,
+    willFilter,
   };
 
   return (
