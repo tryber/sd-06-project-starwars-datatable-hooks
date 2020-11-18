13d9965fbeab73980b6e9edfefb96269d14ee174
Jean Machado <jeanvictor.arq@gmail.com>
2020-11-18 16:13:47 -0300

    requisito 1 ainda não

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -27,21 +27,22 @@ const Table = () => {
         </thead>
         <tbody>
           { !stateStarWars.data ? <Loading />
-            : stateStarWars.data.results.map((planet) => (<tr key={ planet.name }>
-              <td>{ planet.name }</td>
-              <td>{ planet.rotation_period }</td>
-              <td>{ planet.orbital_period }</td>
-              <td>{ planet.diameter }</td>
-              <td>{ planet.climate }</td>
-              <td>{ planet.gravity }</td>
-              <td>{ planet.terrain }</td>
-              <td>{ planet.surface_water }</td>
-              <td>{ planet.population }</td>
-              <td>{ planet.films.length }</td>
-              <td>{ planet.created }</td>
-              <td>{ planet.edited }</td>
-              <td>{ planet.url }</td>
-            </tr>))}
+            : stateStarWars.data.results.map((planet) =>
+              (<tr key={ planet.name }>
+                <td>{ planet.name }</td>
+                <td>{ planet.rotation_period }</td>
+                <td>{ planet.orbital_period }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films.length }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>{ planet.url }</td>
+              </tr>))}
         </tbody>
       </table>
     </StyledTable>
