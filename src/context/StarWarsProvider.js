74fc9e7daa39ab56ce241b803dbfd542243c28d9
Leandro <leandro.feitoza8018@gmail.com>
2020-11-18 14:39:58 -0300

    Feito a configuração do provider para receber os dados da API

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import starWarsContext from './starWarsContext';
+
+import fetchStarWarsPlanets from '../services/fetchApi';
 
 function StarWarsProvider({ children }) {
   const [data, setData] = useState([]);
+
+  const getStarWarsPlanets = async () => {
+    const resultsFromApi = await fetchStarWarsPlanets();
+    setData(resultsFromApi);
+  };
+
+  getStarWarsPlanets();
 
   return (
     <starWarsContext.Provider value={ { data } }>
@@ -10,5 +20,9 @@ function StarWarsProvider({ children }) {
     </starWarsContext.Provider>
   );
 }
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export default StarWarsProvider;
