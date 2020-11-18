5fc892eb5e6b9dd12070e56ff3eb683525eaca96
alcantara2509 <alcantara2509@gmail.com>
2020-11-17 23:50:43 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Table from './components/Table';
+import getPlanets from './api';
+import StarWarsContext from './context/StarWarsContext';
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiResponse = await getPlanets();
+      setData(apiResponse.results);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>app</h1>
-      <div />
-    </>
+    <StarWarsContext.Provider value={ data }>
+      <Table />
+    </StarWarsContext.Provider>
   );
 }
 
