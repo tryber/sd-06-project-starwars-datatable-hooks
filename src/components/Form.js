b266fe0c34dbec2869a22da1d211dcb22bbb26fb
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-11-18 18:16:27 -0300

    add numeric filters

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,26 +2,129 @@ import React, { useContext, useEffect, useState } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function Form() {
-  const { data, setDataShowed } = useContext(StarWarsContext);
-  const [inputText, setInputText] = useState('');
+  const { data, dataShowed, setDataShowed } = useContext(StarWarsContext);
+  const [nameFilter, setNameFilter] = useState('');
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState('');
+  const [filtersSaved, setFiltersSaved] = useState([]);
 
-  const handleChange = ({ target }) => {
-    setInputText(target.value);
+  const handleChangeName = ({ target }) => {
+    setNameFilter(target.value);
+  };
+
+  const handleChangeColumn = ({ target }) => {
+    setColumnFilter(target.value);
+  };
+
+  const handleChangeComparison = ({ target }) => {
+    setComparisonFilter(target.value);
+  };
+
+  const handleChangeValue = ({ target }) => {
+    setValueFilter(target.value);
   };
 
   useEffect(() => {
-    const dataToBeShowed = [...data].filter((planet) => planet.name.includes(inputText));
+    const dataToBeShowed = [...data].filter((planet) => planet.name.includes(nameFilter));
     setDataShowed(dataToBeShowed);
-  }, [inputText]);
+  }, [nameFilter]);
+
+  const applyFilters = (inputedFilter) => {
+    if (inputedFilter.comparison === 'igual a') {
+      const dataToBeShowed = [...dataShowed]
+        .filter((planet) => (
+          parseFloat(planet[inputedFilter.column]) === parseFloat(inputedFilter.value)
+        ));
+      setDataShowed(dataToBeShowed);
+    }
+    if (inputedFilter.comparison === 'maior que') {
+      const dataToBeShowed = [...dataShowed]
+        .filter((planet) => (
+          parseFloat(planet[inputedFilter.column]) > parseFloat(inputedFilter.value)
+        ));
+      setDataShowed(dataToBeShowed);
+    }
+    if (inputedFilter.comparison === 'menor que') {
+      const dataToBeShowed = [...dataShowed]
+        .filter((planet) => (
+          parseFloat(planet[inputedFilter.column]) < parseFloat(inputedFilter.value)
+        ));
+      setDataShowed(dataToBeShowed);
+    }
+  };
+
+  const saveFilters = (e) => {
+    e.preventDefault();
+    const inputedFilters = {};
+    inputedFilters.column = columnFilter;
+    inputedFilters.comparison = comparisonFilter;
+    inputedFilters.value = valueFilter;
+    setFiltersSaved(filtersSaved.concat(inputedFilters));
+    applyFilters(inputedFilters);
+  };
 
   return (
     <div>
-      <input
-        type="text"
-        data-testid="name-filter"
-        value={ inputText }
-        onChange={ handleChange }
-      />
+      <form>
+        <input
+          type="text"
+          data-testid="name-filter"
+          value={ nameFilter }
+          name="name"
+          onChange={ handleChangeName }
+        />
+        <select
+          data-testid="column-filter"
+          name="columns"
+          value={ columnFilter }
+          onChange={ handleChangeColumn }
+          required
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          value={ comparisonFilter }
+          onChange={ handleChangeComparison }
+          required
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          value={ valueFilter }
+          name="value"
+          onChange={ handleChangeValue }
+          required
+        />
+        <button
+          type="submit"
+          onClick={ saveFilters }
+          data-testid="button-filter"
+        >
+          Adicionar filtros
+        </button>
+      </form>
+      {filtersSaved.map((filter) => (
+        <div key={ filtersSaved.length }>
+          <p>
+            { filter.column }
+            {' '}
+            { filter.comparison}
+            {' '}
+            {filter.value}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
