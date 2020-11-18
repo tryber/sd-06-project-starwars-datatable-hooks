57a47db3ad84c6c057fbefa8ec6566760d5d9c2f
kissyla <kissylamottapereira@gmail.com>
2020-11-18 16:55:38 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
+import PlanetFilter from './components/PlanetFilter';
 import Provider from './context/StarWarsProvider';
 
 function App() {
   return (
     <Provider>
+      <PlanetFilter />
       <Table />
     </Provider>
   );
