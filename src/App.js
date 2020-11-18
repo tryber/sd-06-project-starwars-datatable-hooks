70c1f3f2ee29277c404d2ddca44fc9605813badf
Jean Machado <jeanvictor.arq@gmail.com>
2020-11-18 13:43:26 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 import StarWarsProvider from './Context/StarWarsProvider';
 
-import Table from './components/Table';
+import TableInformatio from './components/Table/';
 
-function App() {
+function Table() {
   return (
     <div>
       <StarWarsProvider>
-        <Table />
+        <TableInformatio />
       </StarWarsProvider>
     </div>
   );
 }
 
-export default App;
+export default Table;
