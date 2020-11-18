440bf81a6caaf27922227cc8bef56f9dac97362e
ilanribeiro <ilanribeiro@gmail.com>
2020-11-18 12:16:47 -0300

    feature: builded context structure

---
+++ src/context/StarWarsProvider.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import StarWarsContext from './StarWarsContext';
+import fetchPlanetsData from '../services/dataServicesAPI';
+
+function StarWarsProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const planets = await fetchPlanetsData();
+      planets.map((orb) => delete orb.residents);
+      console.log('planets provider', planets);
+      setData(planets);
+    };
+    getPlanets();
+  }, []);
+
+  return (
+    <StarWarsContext.Provider value={ { data, setData } }>
+      { children }
+    </StarWarsContext.Provider>
+  );
+}
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default StarWarsProvider;
