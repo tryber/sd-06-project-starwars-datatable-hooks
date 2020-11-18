71f1d0bbbb3ed055e855075d18898a3098002c91
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-11-18 18:12:53 -0300

    save

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import './App.css';
 function App() {
   const [planets, setPlanets] = useState('');
   const [name, setName] = useState('');
+  const [column, setColumn] = useState('rotation_period');
+  const [comparison, setComparison] = useState('>');
+  const [value, setValue] = useState(0);
+  const [btnFilter, setBtnFilter] = useState(false);
   const state = {
     planets,
     setPlanets,
+    btnFilter,
+    setBtnFilter,
     filters: {
       filterByName: {
         name,
         setName,
       },
+      filterByNumericValues: [
+        {
+          column,
+          setColumn,
+          comparison,
+          setComparison,
+          value,
+          setValue,
+        }
+      ]
     },
   };
   return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,20 +4,22 @@ import '../App.css';
 import PlanetContext from '../context/PlanetContext';
 
 function Home() {
-  const { planets, setPlanets, filters: { filterByName } } = useContext(PlanetContext);
+  const {
+    planets, setPlanets, btnFilter, setBtnFilter, filters: { filterByName, filterByNumericValues: [{ column, setColumn, comparison, setComparison, value, setValue }] },
+  } = useContext(PlanetContext);
 
-  const keys = [
+  const columnTitle = [
     'name', 'rotation_period', 'orbital_period', 'diameter', 'climate', 'gravity',
     'terrain', 'surface_water', 'population', 'films', 'created', 'edited', 'url',
+  ];
+
+  const columnFilter = [
+    'rotation_period', 'orbital_period', 'diameter', 'surface_water', 'population',
   ];
 
   useEffect(() => {
     fetchApi(setPlanets);
   }, []);
-
-  const handleChange = (({ target }) => {
-    filterByName.setName(target.value);
-  });
 
   return (
     <div className="App">
@@ -29,19 +31,47 @@ function Home() {
             data-testid="name-filter"
             id="nameFilter"
             value={ filterByName.name }
-            onChange={ handleChange }
+            onChange={({target}) => filterByName.setName(target.value)}
           />
         </label>
+        <label htmlFor="filters">
+          Escolha o campo:
+          <select data-testid="column-filter" name="filters" value={column} onChange={({target}) => setColumn(target.value)}>
+            {columnFilter.map((filters) => (
+              <option key={filters} value={filters}>{filters}</option>
+            ))}
+          </select>
+          <select data-testid="comparison-filter" name="filters" value={comparison} onChange={({target}) => setComparison(target.value) }>
+            <option value=">">maior que</option>
+            <option value="<">menor que</option>
+            <option value="==">igual a</option>
+          </select>
+          <input type="number" data-testid="value-filter" value={value} onChange={({target}) => setValue(target.value)} />
+        </label>
+        <button type="button" data-testid="button-filter" onClick={() => setBtnFilter((btnFilter === true) ? false : true)}>Filtrar</button>
       </form>
       <table>
         <thead>
           <tr>
-            {keys.map((title) => <th key={ title }>{ title }</th>)}
+            {columnTitle.map((title) => <th key={ title }>{ title }</th>)}
           </tr>
         </thead>
         <tbody>
           {(!planets) ? <h1>LOADING...</h1>
             : planets.filter((name) => name.name.match(filterByName.name))
+            .filter((filter) => {
+              if (btnFilter === true) {
+                let result = false;
+                if(comparison === '>') {
+                  result = filter[column] > parseInt(value);
+                } else if (comparison === '<') {
+                  result = filter[column] < parseInt(value);
+                } else if (comparison === '==') {
+                  result = filter[column] == parseInt(value);
+                }
+                return result
+              }
+            })
               .map((item) => (
                 <tr key={ item.name }>
                   <td>{ item.name }</td>
@@ -57,6 +87,7 @@ function Home() {
                   <td>{ item.created }</td>
                   <td>{ item.edited }</td>
                   <td>{ item.url }</td>
+                  {console.log(value)}
                 </tr>
               ))}
         </tbody>
