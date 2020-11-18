a590f842a13421490ca7006e70652d19c92edfbd
Marcelo Pessini <marceloppessini@gmail.com>
2020-11-18 13:52:50 -0300

    reset filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -19,24 +19,35 @@ function Filter() {
     setNumericFilter(filterCreation);
   };
 
+  const resetFilter = () => {
+    const filterCreation = { column: '',
+      comparison: '',
+      value: '',
+    };
+    setNumericFilter(filterCreation);
+  };
+
   const colunmOptions = ['population', 'orbital_period', 'diameter',
     'rotation_period', 'surface_water'];
 
   return (
     <div>
       <input onChange={ handleChange } type="text" data-testid="name-filter" />
-      <select data-testid="column-filter" id="column">
-        <option hidden value="">Selecione</option>
-        {colunmOptions
-          .map((option) => <option value={ option } key={ option }>{option}</option>)}
-      </select>
-      <select data-testid="comparison-filter" id="comparison">
-        <option hidden value="">Selecione</option>
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input data-testid="value-filter" type="number" id="number" />
+      <div data-testid="filter">
+        <select data-testid="column-filter" id="column">
+          <option hidden value="">Selecione</option>
+          {colunmOptions
+            .map((option) => <option value={ option } key={ option }>{option}</option>)}
+        </select>
+        <select data-testid="comparison-filter" id="comparison">
+          <option hidden value="">Selecione</option>
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input data-testid="value-filter" type="number" id="number" />
+        <button type="button" onClick={ resetFilter }>X</button>
+      </div>
       <button
         data-testid="button-filter"
         type="button"
