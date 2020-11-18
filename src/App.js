aff9cdb9def6b123c9b34d4317ce8e39dfe4fda0
Forna <fornagieri.artur@gmail.com>
2020-11-18 14:03:33 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// Componentes
+import Table from './components/Table';
+
+// Providers
+import Provider from './providers/Provider';
+
+function App() { // o App é minha home
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <main>
+        <Table />
+      </main>
+    </Provider>
   );
 }
 
