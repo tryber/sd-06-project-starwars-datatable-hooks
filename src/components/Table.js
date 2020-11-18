ae6ce25b2f2a2317b2a9b8bf19fcffb0b73166a2
ilanribeiro <ilanribeiro@gmail.com>
2020-11-18 15:00:48 -0300

    feature: implemented the fill table with fetch data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+// import PropTypes from 'prop-types';
 
 import StarWarsContext from '../context/StarWarsContext';
 
