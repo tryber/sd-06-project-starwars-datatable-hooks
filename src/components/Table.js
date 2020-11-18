335a70c0f531397a5193473e58643ab9c96b8e1c
Julia Porto <juportob@gmail.com>
2020-11-18 15:54:53 -0200

    2nd requirement - fixing linter

--- src/components/Table.js
+++ src/components/Table.js
@@ -31,25 +31,23 @@ function Table() {
         {data
           .filter((e) => e.name.toLowerCase()
             .includes(name.toLowerCase()))
-            .map((planet) => (
-              <tr key={ planet }>
-                <td key={ planet.name }>{ planet.name }</td>
-                <td key={ planet.rotation_period }>{ planet.rotation_period }</td>
-                <td key={ planet.orbital_period }>{ planet.orbital_period }</td>
-                <td key={ planet.diameter }>{ planet.diameter }</td>
-                <td key={ planet.climate }>{ planet.climate }</td>
-                <td key={ planet.gravity }>{ planet.gravity }</td>
-                <td key={ planet.terrain }>{ planet.terrain }</td>
-                <td key={ planet.surface_water }>{ planet.surface_water }</td>
-                <td key={ planet.population }>{ planet.population }</td>
-                <td key={ planet.films }>{ planet.films }</td>
-                <td key={ planet.created }>{ planet.created }</td>
-                <td key={ planet.edited }>{ planet.edited }</td>
-                <td key={ planet.url }>{ planet.url }</td>
-              </tr>
-            )
-          )
-        }
+          .map((planet) => (
+            <tr key={ planet }>
+              <td key={ planet.name }>{ planet.name }</td>
+              <td key={ planet.rotation_period }>{ planet.rotation_period }</td>
+              <td key={ planet.orbital_period }>{ planet.orbital_period }</td>
+              <td key={ planet.diameter }>{ planet.diameter }</td>
+              <td key={ planet.climate }>{ planet.climate }</td>
+              <td key={ planet.gravity }>{ planet.gravity }</td>
+              <td key={ planet.terrain }>{ planet.terrain }</td>
+              <td key={ planet.surface_water }>{ planet.surface_water }</td>
+              <td key={ planet.population }>{ planet.population }</td>
+              <td key={ planet.films }>{ planet.films }</td>
+              <td key={ planet.created }>{ planet.created }</td>
+              <td key={ planet.edited }>{ planet.edited }</td>
+              <td key={ planet.url }>{ planet.url }</td>
+            </tr>
+        ))}
       </tbody>
     </table>
   );
