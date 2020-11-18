16351847824699bf5019e161e771a3de34df3136
Cássio Rubens <cassiorubensadv@gmail.com>
2020-11-18 14:51:42 -0300

    FIX: eslint

--- src/components/Table.js
+++ src/components/Table.js
@@ -18,23 +18,12 @@ function Table() {
       <tbody>
         {planets
           .filter((planet) => planet.name.toLowerCase()
-            .includes(filterPlanet.toLowerCase())).map((planet) => (
+            .includes(filterPlanet.toLowerCase()))
+          .map((planet) => (
             <tr key={ planet.name }>
-              <td>{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.population}</td>
-              <td>{planet.films}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-              <td>{planet.url}</td>
-            </tr>
-          ))}
+              {headers
+                .map((header) => <td key={ planet[header] }>{planet[header]}</td>)}
+            </tr>))}
       </tbody>
     </table>
   );
