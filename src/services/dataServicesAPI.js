418db98133f54ae435c684da2a909dbf0c9ceb5c
ilanribeiro <ilanribeiro@gmail.com>
2020-11-18 16:37:05 -0300

    Eslint

--- src/services/dataServicesAPI.js
+++ src/services/dataServicesAPI.js
@@ -1,9 +1,7 @@
 const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
-const fetchPlanetsData = () => {
-  return fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => data.results);
-};
+const fetchPlanetsData = () => fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => data.results);
 
 export default fetchPlanetsData;
