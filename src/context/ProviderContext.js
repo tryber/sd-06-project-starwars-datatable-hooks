eb6bc909aa0c2d587509086c7310faa4d89798d8
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-11-18 19:46:55 -0300

    lint resolvido

--- src/context/ProviderContext.js
+++ src/context/ProviderContext.js
@@ -8,7 +8,6 @@ function ProviderContext({ children }) {
   const [name, setName] = useState('');
   const [filterByNumericValues, setFilterByNumericValues] = useState([]);
 
-  // useEffect(() => console.log(filterByNumericValues), [filterByNumericValues])
   const [arrayOptionsColumn, setArrayOptionsColumn] = useState([
     'population',
     'orbital_period',
@@ -20,7 +19,6 @@ function ProviderContext({ children }) {
   useEffect(() => {
     const objVoid = 0;
     if (Object.keys(data).length !== objVoid) {
-      // console.log('chamou mudou copyDataResults')
       let copyDataResults = data.results.map((result) => ({ ...result }));
       copyDataResults.forEach((objPlanet) => {
         delete objPlanet.residents;
@@ -33,47 +31,36 @@ function ProviderContext({ children }) {
       }
       const arrayVoid = 0;
       if (filterByNumericValues.length > arrayVoid) {
-        const filterByNumber1 = filterByNumericValues.reduce((total, { column, comparison, value }, index) => {
-          // console.log('super test', (index !== 0 ? console.log('total foi') : console.log('filterByName foi')))
-          const filterByNumber2 = (index === 0 ? copyDataResults : total).filter((objPlanet) => {
-            setArrayOptionsColumn(arrayOptionsColumn
-              .filter((optionColumn) => optionColumn !== column));
-            let boolTest = false;
-            switch (comparison) {
-            case 'maior que':
-              boolTest = parseInt(objPlanet[column], 10) > parseInt(value, 10);
-              break;
-            case 'menor que':
-              boolTest = parseInt(objPlanet[column], 10) < parseInt(value, 10);
-              break;
-            case 'igual a':
-              boolTest = parseInt(objPlanet[column], 10) === parseInt(value, 10);
-              break;
-            default:
-              break;
-            }
-            return boolTest;
-          });
-          // console.log('total', total)
-          // console.log('filterByNumber2', filterByNumber2)
-          return filterByNumber2;
-        }, []);
+        const filterByNumber1 = filterByNumericValues
+          .reduce((total, { column, comparison, value }, index) => {
+            const firstCall = 0;
+            const filterByNumber2 = (index === firstCall ? copyDataResults : total)
+              .filter((objPlanet) => {
+                setArrayOptionsColumn(arrayOptionsColumn
+                  .filter((optionColumn) => optionColumn !== column));
+                let boolTest = false;
+                switch (comparison) {
+                case 'maior que':
+                  boolTest = parseInt(objPlanet[column], 10) > parseInt(value, 10);
+                  break;
+                case 'menor que':
+                  boolTest = parseInt(objPlanet[column], 10) < parseInt(value, 10);
+                  break;
+                case 'igual a':
+                  boolTest = parseInt(objPlanet[column], 10) === parseInt(value, 10);
+                  break;
+                default:
+                  break;
+                }
+                return boolTest;
+              });
+            return filterByNumber2;
+          }, []);
         copyDataResults = filterByNumber1;
       }
       setCopyDataResultsTable(copyDataResults);
     }
-    // console.log('camou data mudou')
   }, [data, name, filterByNumericValues]);
-
-  // useEffect(() => {
-  //   if (name !== '') {
-  //     const nameUpperCase = name.toUpperCase();
-  //     const filterByName = copyDataResultsTable
-  //       .filter((objPlanet) => objPlanet.name.toUpperCase().includes(nameUpperCase));
-  //     setCopyDataResultsTable(filterByName);
-  //   }
-  //   console.log("chamou name")
-  // }, [name]);
 
   return (
     <StarWarsContext.Provider
