bfbd77df97ed047b5bac5e9592a4fd1de54a8399
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-18 20:47:23 -0300

    [pap contextApi] simulação de planeta:
    Simula um planeta, para criar dinamicamente a tabela e os dados da mesma

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,37 +1,68 @@
 import React from 'react';
 
 function Table() {
+  const mockPlanetForTable = [{
+    name: 'Naboo',
+    rotation_period: '26',
+    orbital_period: '312',
+    diameter: '12120',
+    climate: 'temperate',
+    gravity: '1 standard',
+    terrain: 'grassy hills, swamps, forests, mountains',
+    surface_water: '12',
+    population: '4500000000',
+    residents: [
+      'https://swapi-trybe.herokuapp.com/api/people/3/',
+      'https://swapi-trybe.herokuapp.com/api/people/21/',
+      'https://swapi-trybe.herokuapp.com/api/people/35/',
+      'https://swapi-trybe.herokuapp.com/api/people/36/',
+      'https://swapi-trybe.herokuapp.com/api/people/37/',
+      'https://swapi-trybe.herokuapp.com/api/people/38/',
+      'https://swapi-trybe.herokuapp.com/api/people/39/',
+      'https://swapi-trybe.herokuapp.com/api/people/42/',
+      'https://swapi-trybe.herokuapp.com/api/people/60/',
+      'https://swapi-trybe.herokuapp.com/api/people/61/',
+      'https://swapi-trybe.herokuapp.com/api/people/66/',
+    ],
+    films: [
+      'https://swapi-trybe.herokuapp.com/api/films/3/',
+      'https://swapi-trybe.herokuapp.com/api/films/4/',
+      'https://swapi-trybe.herokuapp.com/api/films/5/',
+      'https://swapi-trybe.herokuapp.com/api/films/6/',
+    ],
+    created: '2014-12-10T11:52:31.066000Z',
+    edited: '2014-12-20T20:58:18.430000Z',
+    url: 'https://swapi-trybe.herokuapp.com/api/planets/8/',
+  }];
   return (
     <div>
-
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            { Object.keys(mockPlanetForTable[0]).filter((key) => key !== 'residents')
+              .map((keys) => (
+                <th key={ keys } scope="col">{keys}</th>
+              ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {mockPlanetForTable.map((mockedPlanet) => (
+            <tr key={ mockedPlanet.created }>
+              <td>{ mockedPlanet.name }</td>
+              <td>{ mockedPlanet.rotation_period }</td>
+              <td>{ mockedPlanet.orbital_period }</td>
+              <td>{ mockedPlanet.diameter }</td>
+              <td>{ mockedPlanet.climate }</td>
+              <td>{ mockedPlanet.gravity }</td>
+              <td>{ mockedPlanet.terrain }</td>
+              <td>{ mockedPlanet.surface_water }</td>
+              <td>{ mockedPlanet.population }</td>
+              <td>{ mockedPlanet.films }</td>
+              <td>{ mockedPlanet.created }</td>
+              <td>{ mockedPlanet.edited }</td>
+              <td>{ mockedPlanet.url }</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
