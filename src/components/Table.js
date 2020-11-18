3454ed2ad9e9ffa1ad4effed257a6cbf9c8eb5c7
Cássio Rubens <cassiorubensadv@gmail.com>
2020-11-17 21:39:09 -0300

    FEAT: Requirement One

---
+++ src/components/Table.js
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+function Table() {
+  const { planets, headers, getPlanetsApi } = useContext(StarWarsContext);
+
+  useEffect(() => {
+    getPlanetsApi();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header) => <th key={ header }>{header}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
