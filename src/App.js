1f85fbaf255604e9edd9436ed8a24b9a58f9f611
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-19 03:17:31 -0300

    Altera o Provider do arquivo index para o App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import StarWarsProvider from './context/StarWarsProvider';
+
 import Header from './components/Header';
 import InputFilterBar from './components/InputFilterBar';
 import Table from './components/Table';
 
 function App() {
   return (
-    <>
+    <StarWarsProvider>
       <Header />
       <InputFilterBar />
       <Table />
-    </>
+    </StarWarsProvider>
   );
 }
 
