3432368660bb46cf23c3107c93670928a6125f56
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-18 22:52:52 -0300

    !!![pap contextApi] useState() deve estar dentro do Provider
    Corrige alocação do useState().
    [erro apontado: Error: Invalid hook call. Hooks can only be called
    inside of the body of a function component.]
    
    Também simula os dados para conferir se a renderização ocorre com
    sucesso

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,52 +5,52 @@ function Table() {
   const { prismPlanets } = useContext(StarWarsContext);
 
   // essas informações abaixo foram simuladas apenas para testar a tabela
-  const mockPlanetForTable = [{
-    name: 'Naboo',
-    rotation_period: '26',
-    orbital_period: '312',
-    diameter: '12120',
-    climate: 'temperate',
-    gravity: '1 standard',
-    terrain: 'grassy hills, swamps, forests, mountains',
-    surface_water: '12',
-    population: '4500000000',
-    residents: [
-      'https://swapi-trybe.herokuapp.com/api/people/3/',
-      'https://swapi-trybe.herokuapp.com/api/people/21/',
-      'https://swapi-trybe.herokuapp.com/api/people/35/',
-      'https://swapi-trybe.herokuapp.com/api/people/36/',
-      'https://swapi-trybe.herokuapp.com/api/people/37/',
-      'https://swapi-trybe.herokuapp.com/api/people/38/',
-      'https://swapi-trybe.herokuapp.com/api/people/39/',
-      'https://swapi-trybe.herokuapp.com/api/people/42/',
-      'https://swapi-trybe.herokuapp.com/api/people/60/',
-      'https://swapi-trybe.herokuapp.com/api/people/61/',
-      'https://swapi-trybe.herokuapp.com/api/people/66/',
-    ],
-    films: [
-      'https://swapi-trybe.herokuapp.com/api/films/3/',
-      'https://swapi-trybe.herokuapp.com/api/films/4/',
-      'https://swapi-trybe.herokuapp.com/api/films/5/',
-      'https://swapi-trybe.herokuapp.com/api/films/6/',
-    ],
-    created: '2014-12-10T11:52:31.066000Z',
-    edited: '2014-12-20T20:58:18.430000Z',
-    url: 'https://swapi-trybe.herokuapp.com/api/planets/8/',
-  }];
+  // const mockPlanetForTable = [{
+  //   name: 'Naboo',
+  //   rotation_period: '26',
+  //   orbital_period: '312',
+  //   diameter: '12120',
+  //   climate: 'temperate',
+  //   gravity: '1 standard',
+  //   terrain: 'grassy hills, swamps, forests, mountains',
+  //   surface_water: '12',
+  //   population: '4500000000',
+  //   residents: [
+  //     'https://swapi-trybe.herokuapp.com/api/people/3/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/21/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/35/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/36/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/37/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/38/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/39/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/42/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/60/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/61/',
+  //     'https://swapi-trybe.herokuapp.com/api/people/66/',
+  //   ],
+  //   films: [
+  //     'https://swapi-trybe.herokuapp.com/api/films/3/',
+  //     'https://swapi-trybe.herokuapp.com/api/films/4/',
+  //     'https://swapi-trybe.herokuapp.com/api/films/5/',
+  //     'https://swapi-trybe.herokuapp.com/api/films/6/',
+  //   ],
+  //   created: '2014-12-10T11:52:31.066000Z',
+  //   edited: '2014-12-20T20:58:18.430000Z',
+  //   url: 'https://swapi-trybe.herokuapp.com/api/planets/8/',
+  // }];
   return (
     <div>
       <table className="table">
         <thead>
           <tr>
-            { Object.keys(mockPlanetForTable[0]).filter((key) => key !== 'residents')
+            { Object.keys(prismPlanets[0]).filter((key) => key !== 'residents')
               .map((keys) => (
                 <th key={ keys } scope="col">{keys}</th>
               ))}
           </tr>
         </thead>
         <tbody>
-          {mockPlanetForTable.map((mockedPlanet) => (
+          {prismPlanets.map((mockedPlanet) => (
             <tr key={ mockedPlanet.created }>
               <td>{ mockedPlanet.name }</td>
               <td>{ mockedPlanet.rotation_period }</td>
