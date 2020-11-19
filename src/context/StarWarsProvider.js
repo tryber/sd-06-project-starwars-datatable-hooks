cba79ad948304fe094706056d7183734db8be222
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-18 21:31:23 -0300

    [pap contextApi] cria o provider
    esboça a estrutura inicial do provider

---
+++ src/context/StarWarsProvider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function StarWarsProvider() {
+  return(
+    // o provider é o componente que agrupa a lógica do contexto.
+  );
+}
+
+export default StarWarsProvider;
