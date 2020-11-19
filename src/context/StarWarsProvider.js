21b29896b5c0e5c0573e69c22d20142f28a8d1f1
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-11-18 21:57:47 -0300

    [pap contextApi] fazendo uso do useState() cria variável que lida com o armazenamento de dados. No array constam o estado e a função para 'setá-lo', uma vez que o React só vai saber que o componente atualizou se ocorrer o setState. O estado inicial - em useState([]) - é um array vazio, já que se trata de uma lista de planetas.

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // o Provider irá prover os dados para os componentes abaixo dele ({ children})
 // na árvore de componentes;
 import StarWarsContext from './StarWarsContext';
+
+// armazenando os dados: lista de planetas. Cria-se uma variável no estado em um
+// componente funcional através do useState();
+const [prismPlanets, setPrismPlanets] = useState([]);
 
 function StarWarsProvider({ children }) {
   return (
