6bdfd281434c52440c606e7fa08cb5e4e30842e4
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-11-21 16:52:46 -0300

    Solving table

--- src/components/TablePlanets.js
+++ src/components/TablePlanets.js
@@ -1,18 +1,76 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function TablePlanets() {
-  const { planets, requestPlanets } = useContext(StarWarsContext);
+  const { contexts } = useContext(StarWarsContext);
+  const { planets, filters } = contexts;
 
-  useEffect(() => {
-    requestPlanets();
-  }, []);
-
-  console.log(planets);
+  const headersTable = [
+    'Name',
+    'Rotation Period',
+    'Orbital Period',
+    'Diameter',
+    'Climate',
+    'Gravity',
+    'Terrain',
+    'Surface Water',
+    'Population',
+    'Films',
+    'Created',
+    'Edited',
+    'Url',
+  ];
 
   return (
     <div>
-      Hello
+      <table>
+        <thead>
+          <tr>
+            {headersTable.map((headers, index) => (
+              <th key={ index }>{headers}</th>))}
+          </tr>
+        </thead>
+        <tbody>
+          {planets
+            .filter((planet) => planet.name.toLowerCase()
+              .includes(filters.filterName.name.toLowerCase())).map(
+              (
+                {
+                  name,
+                  rotation_period: rotationPeriod,
+                  orbital_period: orbitalPeriod,
+                  diameter,
+                  climate,
+                  gravity,
+                  terrain,
+                  surface_water: surfaceWater,
+                  population,
+                  films,
+                  created,
+                  edited,
+                  url,
+                },
+                index,
+              ) => (
+                <tr key={ index }>
+                  <td>{name}</td>
+                  <td>{rotationPeriod}</td>
+                  <td>{orbitalPeriod}</td>
+                  <td>{diameter}</td>
+                  <td>{climate}</td>
+                  <td>{gravity}</td>
+                  <td>{terrain}</td>
+                  <td>{surfaceWater}</td>
+                  <td>{population}</td>
+                  <td>{films.length}</td>
+                  <td>{created}</td>
+                  <td>{edited}</td>
+                  <td>{url}</td>
+                </tr>
+              ),
+            )}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,24 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import apiPlanets from '../services';
 import StarWarsContext from './StarWarsContext';
 
 function PlanetsProvider({ children }) {
-  const [planets, setPlanets] = useState();
+  const [planets, setPlanets] = useState([]);
+  const [filters, setFilters] = useState({
+    filterName: { name: '' },
+    // filterByNumericValues: [],
+    // order: { column: 'Name', sort: 'ASC' },
+  });
 
-  async function requestPlanets() {
-    const planetsResult = await apiPlanets();
-    setPlanets(planetsResult);
-  }
+  useEffect(() => {
+    async function requestPlanets() {
+      const planetsResult = await apiPlanets();
+      setPlanets(planetsResult);
+    }
+    requestPlanets();
+  }, []);
+
+  const filteringName = (name) => {
+    setFilters({
+      ...filters,
+      filterName: { name },
+    });
+  };
+
+  const contexts = {
+    planets,
+    filters,
+    setFilters,
+    filteringName,
+  };
+
   return (
-    <StarWarsContext.Provider value={ { planets, requestPlanets } }>
+    <StarWarsContext.Provider value={ { contexts } }>
       {children}
     </StarWarsContext.Provider>
   );
 }
 
 PlanetsProvider.propTypes = {
-  children: PropTypes.elementType.isRequired,
-};
+  children: PropTypes.arrayOf(Object).isRequired,
+}.isRequired;
 
 export default PlanetsProvider;
