419d7d136d91d3c62bee7d74ab4d62c9bd443a26
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2020-11-22 15:10:55 -0300

    [Req.1] Table headers fix

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Table = ({ data: planets }) => {
-  // const columns = Object.keys(planets[0]).filter((key) => key !== 'residents');
-  const columns = [
-    'Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate', 'Gravity',
-    'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edited', 'URL',
+  // const headers = Object.keys(planets[0]).filter((key) => key !== 'residents');
+  const headers = [
+    'Name', 'Rotation_Period', 'Orbital_Period', 'Diameter', 'Climate', 'Gravity',
+    'Terrain', 'Surface_Water', 'Population', 'Films', 'Created', 'Edited', 'URL',
   ];
 
   return (
     <table>
       <thead>
         <tr>
-          { columns.map((col, index) => (<th key={ index }>{ col }</th>)) }
+          { headers.map((col, index) => (<th key={ index }>{ col }</th>)) }
         </tr>
       </thead>
       <tbody>
@@ -20,7 +20,7 @@ const Table = ({ data: planets }) => {
           planets.map((planet, index) => (
             <tr key={ index }>
               {
-                columns.map((cat, i) => (
+                headers.map((cat, i) => (
                   <td key={ i }>{ planet[cat.toLocaleLowerCase()] }</td>
                 ))
               }
