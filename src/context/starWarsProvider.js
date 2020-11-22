4f538db8fb7a10ed8bce4152358df1e43486987c
Anderson <andersoni.trybe@gmail.com>
2020-11-22 17:24:38 -0300

    Provider constructor

--- src/context/starWarsProvider.js
+++ src/context/starWarsProvider.js
@@ -1,12 +1,20 @@
 import React, {} from 'react';
 import PropTypes from 'prop-types';
+import starWarsContext from './starWarsContext';
 
 function starWarsProvider({ children }) {
 
+  return (
+    <starWarsContext.Provider
+      value={ data }
+    >
+      { children }
+    </starWarsContext.Provider>
+  );
 }
 
 starWarsProvider.propTypes = {
   children: PropTypes.shape().isRequired,
-}
+};
 
 export default starWarsProvider;
