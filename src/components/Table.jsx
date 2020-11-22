85e043a1ae23de74ce4c1d6f0db91a3ac5e7d9c6
Anderson <andersoni.trybe@gmail.com>
2020-11-22 15:45:22 -0300

    Ajuste de fetch da API

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import fetchApi from '../services/SWPApi';
 
 const Table = () => {
-  const data = fetchApi();
+  
   return (
     <div>
       Dados
--- src/services/SWPApi.js
+++
@@ -1,9 +0,0 @@
-const API_URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
-
-const fetchApi = async () => {
-  const response = await fetch(API_URL);
-  const json = await response.json();
-  return json.results;
-};
-
-export default fetchApi;
