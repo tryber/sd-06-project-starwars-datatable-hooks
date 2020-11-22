f46b6f4b8e7e2ee4fcd74b01a24f0ce138452d99
Anderson <andersoni.trybe@gmail.com>
2020-11-22 17:21:32 -0300

    Pre build Provider

---
+++ src/context/starWarsProvider.js
@@ -0,0 +1,12 @@
+import React, {} from 'react';
+import PropTypes from 'prop-types';
+
+function starWarsProvider({ children }) {
+
+}
+
+starWarsProvider.propTypes = {
+  children: PropTypes.shape().isRequired,
+}
+
+export default starWarsProvider;
