ba29efbd5997cb89bfa274b96c21f6019fd6715d
Anderson <andersoni.trybe@gmail.com>
2020-11-22 15:06:11 -0300

    Criada tabela para os planetas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
-      Start Wars planets
+      Start Wars Planet Search
       <Table />
     </div>
   );
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import fetchApi from '../services/SWPApi';
 
-const data = fetchApi;
-console.log(data);
-
 const Table = () => {
+  const data = fetchApi();
   return (
     <div>
       Dados
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 };
