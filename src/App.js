52234d3cd121640baa3037fb303b2f0ecb326057
Paulo Frazão <paulo4187@gmail.com>
2020-11-23 13:32:41 -0300

    Req: 03 Filter NUmber

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Table from './components/Table';
 import EntryText from './components/EntryText';
 
 function App() {
+  // const filterIniti = {
+  //   filters: {
+  //     filterByName: {
+  //       name: '',
+  //     },
+  //   },
+  // };
+  // const [filtersName, setFilterName] = useState(filterIniti);
   const [planetsList, setPlanetsList] = useState([]);
   const [planetsData, setPlanetsData] = useState([]);
   const [entryText, setEntryText] = useState('');
@@ -30,6 +38,26 @@ function App() {
     }
   }
 
+  function filtersSelectNumbers(col, comparison, value) {
+    if (col !== null && comparison !== null && value !== null) {
+      if (comparison === 'maior que') {
+        const filterForNumber = planetsList
+          .filter((position) => Number(position[col]) > value);
+        setPlanetsData(filterForNumber);
+      } else if (comparison === 'menor que') {
+        const filterForNumber = planetsList
+          .filter((position) => Number(position[col]) < value);
+        setPlanetsData(filterForNumber);
+      } else if (comparison === 'igual a') {
+        const filterForNumber = planetsList
+          .filter((position) => Number(position[col]) === Number(value));
+        setPlanetsData(filterForNumber);
+      }
+    } else {
+      setPlanetsData(planetsList);
+    }
+  }
+
   function handleOnchange({ target }) {
     const inputValue = target.value;
     setEntryText(inputValue);
@@ -42,6 +70,7 @@ function App() {
         {
           data: planetsData,
           handle: handleOnchange,
+          filtersFunc: filtersSelectNumbers,
           text: entryText }
       }
     >
--- src/components/EntryText.js
+++ src/components/EntryText.js
@@ -1,8 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function EntryText() {
-  const { handle, entryText } = useContext(StarWarsContext);
+  const initFiters = {
+    filterByNumericValues:
+      {
+        column: '',
+        comparison: '',
+        value: '',
+      },
+  };
+  const { handle, entryText, filtersFunc } = useContext(StarWarsContext);
+  const [filters, setFilters] = useState(initFiters);
+
+  function handleChange(event, input) {
+    const filter = event.target.value;
+    setFilters((prevState) => (
+      { filterByNumericValues: { ...prevState.filterByNumericValues, [input]: filter },
+      }));
+    // this.setState({
+    //   form: { ...form, [input]: event.target.value },
+    // });
+    // if (input === 'currency') {
+    //   const curName = currencys.filter((curren) => curren.code === event.target.value)
+    //     .flatMap((currName) => currName.name);
+    //   this.setState({ form:
+    //     { ...form, currencyName: curName[0], [input]: event.target.value },
+    //   });
+    // }
+  }
+
+  function handleClick() {
+    const { value, column, comparison } = filters.filterByNumericValues;
+    filtersFunc(column, comparison, value);
+    console.log(comparison + column + value);
+  }
 
   return (
     <div>
@@ -16,6 +48,65 @@ function EntryText() {
           onChange={ handle }
         />
       </label>
+      <label htmlFor="col-filter">
+        selcione um coluna
+        <select
+          data-testid="column-filter"
+          id="col-filter"
+          onChange={ (event) => handleChange(event, 'column') }
+        >
+          <option value="population">
+            population
+          </option>
+          <option value="orbital_period">
+            orbital_period
+          </option>
+          <option value="diameter">
+            diameter
+          </option>
+          <option value="rotation_period">
+            rotation_period
+          </option>
+          <option value="surface_water">
+            surface_water
+          </option>
+        </select>
+      </label>
+
+      <label htmlFor="faixa-filter">
+        faixa de valor
+        <select
+          data-testid="comparison-filter"
+          id="faixa-filter"
+          onChange={ (event) => handleChange(event, 'comparison') }
+        >
+          <option value="maior que">
+            maior que
+          </option>
+          <option value="menor que">
+            menor que
+          </option>
+          <option value="igual a">
+            igual a
+          </option>
+        </select>
+      </label>
+      <label htmlFor="enter-number">
+        <input
+          data-testid="value-filter"
+          id="enter-number"
+          type="number"
+          placeholder="digite um valor"
+          onChange={ (event) => handleChange(event, 'value') }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleClick }
+      >
+        Filtrar
+      </button>
     </div>);
 }
 
