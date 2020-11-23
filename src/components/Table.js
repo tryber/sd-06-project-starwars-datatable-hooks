e4a89a52f121ade1b3882785ce7a9d3795a66cbd
Anderson <andersoni.trybe@gmail.com>
2020-11-23 10:44:05 -0300

    Criada tabela com a lista de planetas

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,10 +3,48 @@ import starWarsContext from '../context/starWarsContext';
 
 function Table() {
   const { planets } = useContext(starWarsContext);
-  console.log(planets);
   return (
     <div>
       Data list:
+      <table>
+        <thead>
+          <tr>
+            <th>Climate</th>
+            <th>Created</th>
+            <th>Diameter</th>
+            <th>Edited</th>
+            <th>Films</th>
+            <th>Gravity</th>
+            <th>Name</th>
+            <th>Orbital Period</th>
+            <th>Population</th>
+            <th>Rotation Period</th>
+            <th>Surface Water</th>
+            <th>Terrain</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+      </table>
+      <tbody>
+        {planets
+          .map((planet, index) => (
+            <tr key={ index }>
+              <td>{planet.climate}</td>
+              <td>{planet.created}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.films}</td>
+              <td>{planet.gravity}</td>
+              <td data-testid="planet-name">{planet.name}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.population}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+      </tbody>
     </div>
   );
 }
