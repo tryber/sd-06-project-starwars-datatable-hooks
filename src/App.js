acdc35cd45a50b70e7650bad413509f1692885e5
Anderson <andersoni.trybe@gmail.com>
2020-11-24 16:20:14 -0300

    Refatorando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import StarWarsProvider from './context/StarWarsProvider';
-import Table from './components/Table';
 
 function App() {
   return (
     <StarWarsProvider>
       <div className="App">
         Star Wars Planet List
-        <Table />
       </div>
     </StarWarsProvider>
   );
