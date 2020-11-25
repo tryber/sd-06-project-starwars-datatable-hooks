e753d68d5138725fa13a4887f92135b1c93f64eb
Isabela Rosa <isabelarosa954@gmail.com>
2020-11-25 12:08:30 -0300

    requisito 4 - implementando um filtro nas options

--- src/components/InputFilters.js
+++ src/components/InputFilters.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
 
 function InputFilters() {
-  const { setFilters } = useContext(StarWarsContext);
+  const { setFilters, filters } = useContext(StarWarsContext);
   const zero = 0;
 
   const [column, setColumn] = useState('');
@@ -17,18 +17,36 @@ function InputFilters() {
     }));
   };
 
+  /* LÓGICA DE FILTRO DAS OPTIONS:
+  implementei aqui a mesma lógica usada pela Ana Capdeville, para fazer sumir as
+  options conforme um novo filtro é criado. Segue o link do PR:
+  https://github.com/tryber/sd-06-project-starwars-datatable-hooks/pull/14/files
+  Graças a ele também consegui arrumar a lógica de setar novos valores ao objeto
+  filters, retirando a lógica anterior de ter um objetoProvider
+  */
+
+  const renderOptions = () => {
+    let options = [
+      ' ', 'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+    ];
+
+    const filteredOption = filters.filterByNumericValues
+      .map((filter) => Object.values(filter)[0]);
+
+    options = options.filter((option) => !filteredOption.includes(option));
+
+    return options;
+  };
+
   return (
     <div>
       <select
         data-testid="column-filter"
         onChange={ (e) => setColumn(e.target.value) }
       >
-        <option>Selecione</option>
-        <option>population</option>
-        <option>orbital_period</option>
-        <option>diameter</option>
-        <option>rotation_period</option>
-        <option>surface_water</option>
+        {renderOptions().map((option) => (
+          <option key={ option }>{ option }</option>
+        ))}
       </select>
       <select
         data-testid="comparison-filter"
@@ -51,6 +69,17 @@ function InputFilters() {
       >
         Filtrar
       </button>
+      <p><b>FILTROS APLICADOS</b></p>
+      { filters.filterByNumericValues.map((filter, index) => (
+        <div key={ index }>
+          <span>
+            { filter.column }
+            { filter.comparison }
+            { filter.value }
+          </span>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
