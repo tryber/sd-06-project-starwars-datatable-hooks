cadddd52c44cdc2d04ec20d5bc1b722aaf44f351
RenatoCostaDev <renatotrybe@gmail.com>
2020-12-01 10:58:53 -0300

    feat: StarWarTable.jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import StarWarProvider from './context/StarWarProvider';
-import logo from './logo.svg';
-import './App.css';
+import StarWarsTable from './components/StarWarsTable';
 
 function App() {
   return (
     <StarWarProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            Edit
-            <code>src/App.js</code>
-            and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StarWarsTable />
     </StarWarProvider>
   );
 }
