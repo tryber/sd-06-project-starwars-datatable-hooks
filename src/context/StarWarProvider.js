98f2ac6ea22ca1f37a14831daa1873f0e219fb74
RenatoCostaDev <renatotrybe@gmail.com>
2020-12-01 11:26:22 -0300

    feat: Carregando o value com data/dataApi

--- src/context/StarWarProvider.js
+++ src/context/StarWarProvider.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import StarWarsAPI from '../service/StarWarsAPI';
 import StarWarsContext from './StarWarsContext';
 
 function StarWarProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  const dataApi = async () => {
+    const dataResponse = await StarWarsAPI();
+    setData(dataResponse);
+  };
+
+  const value = {// Todos children terão acesso ao data/dataApi
+    data,
+    dataApi,
+  };
+
   return (
-    <StarWarsContext.Provider>
+    <StarWarsContext.Provider value={ value }>
       { children }
     </StarWarsContext.Provider>
-
   );
 }
 
--- src/service/StarWarsAPI.js
+++ src/service/StarWarsAPI.js
@@ -1,5 +1,5 @@
 export default function StarWarsAPI() {
   return fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-    .then((results) => results.json())
-    .then((data) => data.results);
+    .then((response) => response.json())
+    .then((data) => data.response);
 }
