716d4a61de9cc6e1ab74d46fac19e31d78aee0f2
RenatoCostaDev <renatotrybe@gmail.com>
2020-12-01 18:03:37 -0300

    feat: Logica para procura do name/termo

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -32,23 +32,25 @@ function StarWarsTable() {
         </tr>
       </thead>
       <tbody>
-        {data.map((element) => (
-          <tr key={ element.name }>
-            <td>{ element.name }</td>
-            <td>{ element.rotation_period }</td>
-            <td>{ element.orbital_period }</td>
-            <td>{ element.diameter }</td>
-            <td>{ element.climate }</td>
-            <td>{ element.gravity }</td>
-            <td>{ element.terrain }</td>
-            <td>{ element.surface_water }</td>
-            <td>{ element.population }</td>
-            <td>{ element.films }</td>
-            <td>{ element.created }</td>
-            <td>{ element.edited }</td>
-            <td>{ element.url }</td>
-          </tr>
-        ))}
+        {data.filter((element) => element.name.toLowerCase()
+          .includes(searchTerm.toLowerCase()))
+          .map((element) => (
+            <tr key={ element.name }>
+              <td>{ element.name }</td>
+              <td>{ element.rotation_period }</td>
+              <td>{ element.orbital_period }</td>
+              <td>{ element.diameter }</td>
+              <td>{ element.climate }</td>
+              <td>{ element.gravity }</td>
+              <td>{ element.terrain }</td>
+              <td>{ element.surface_water }</td>
+              <td>{ element.population }</td>
+              <td>{ element.films }</td>
+              <td>{ element.created }</td>
+              <td>{ element.edited }</td>
+              <td>{ element.url }</td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
