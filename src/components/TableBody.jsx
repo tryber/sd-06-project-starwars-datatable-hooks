45398dbd1d12e15129d4ab6cfff67abf2d8b0b28
William Gomes <williamjog@hotmail.com>
2020-12-04 09:49:32 -0300

    Linter

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -6,10 +6,12 @@ const filterByNumber = (planets, filter) => {
   if (filter.comparison === 'maior que') {
     return planets
       .filter((planet) => Number(planet[filter.column]) > Number(filter.value));
-  } if (filter.comparison === 'menor que') {
+  }
+  if (filter.comparison === 'menor que') {
     return planets
       .filter((planet) => Number(planet[filter.column]) < Number(filter.value));
-  } if (filter.comparison === 'igual a') {
+  }
+  if (filter.comparison === 'igual a') {
     return planets
       .filter((planet) => Number(planet[filter.column]) === Number(filter.value));
   }
