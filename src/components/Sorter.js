808a5512ff205304effc333640f4b23aca32993b
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-12-22 11:57:56 -0300

    Implemented headers option at Sorter component

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
 
 function Sorter() {
+  const { tableHeaders } = useContext(StarWarsContext);
   return (
     <div>
-      Sorter Component
+      <label htmlFor="column-sort">
+        Order by
+        <select
+          id="column-sort"
+          name="column-sort"
+          data-testid="column-sort"
+        >
+          {tableHeaders.map((header, index) => (
+            <option key={ index } value={ header }>{ header }</option>
+          ))}
+        </select>
+      </label>
     </div>
   );
 }
