8f9c20ac274ec1b2ded241fa3b5a168406a9d353
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2020-12-24 08:23:38 -0300

    iniciando projeto

---
+++ src/components/PlanetTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';\
+
+export default function PlanetTable() {
+  return (
+    <div className="planet-table">
+      area da tabela dos planetas
+    </div>
+  )
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Search() {
+  return (
+    <div className="search">
+      local de procura
+    </div>
+  );
+}
