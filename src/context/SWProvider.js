0a0fc3e93ad783ec1edaa5a72c2d23f1fdcacfe6
DekoPossas <andrepossaster@gmail.com>
2021-01-02 03:02:58 -0300

    esqueci de impoirtar o provider

--- src/context/SWProvider.js
+++ src/context/SWProvider.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import SWContext from './SWContext';
 import fetchPlanetsAPI from '../services/sWservices';
 
