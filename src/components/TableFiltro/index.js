9aaf56ed36841587c29f676bf23c06cc3855d619
Jean Machado <jeanvictor.arq@gmail.com>
2021-01-03 17:08:57 -0300

    add button x

--- src/components/TableFiltro/index.js
+++ src/components/TableFiltro/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from '../../Context/StarWarsContext';
 
 import StyledFilter from './styled';
