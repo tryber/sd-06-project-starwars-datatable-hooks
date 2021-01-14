e4a1ac921d8eaf14f619e60b00946ef854e8c048
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-14 12:36:06 -0300

    Requisite 06 - push 03

--- src/components/Table.js
+++ src/components/Table.js
@@ -83,7 +83,7 @@ function Table() {
             .sort((a, b) => sortSelectedColumn(a, b))
             .map((planet) => (
               <tr key={ planet.name }>
-                <td>{ planet.name }</td>
+                <td data-testid="planet-name">{ planet.name }</td>
                 <td>{ planet.rotation_period }</td>
                 <td>{ planet.orbital_period }</td>
                 <td>{ planet.diameter }</td>
